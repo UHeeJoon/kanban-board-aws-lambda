a2ecc99b42b191add190378c793b9c12486d0ca2
UHeeJoon <yhjyhj3855@gmail.com>
2024-05-06 18:38:52 +0900

    feat: dynamoDB연결 및 데이터 가져오기

--- lambda/getCards.js
+++ lambda/getCards.js
@@ -1,9 +1,38 @@
-export const handler = async (event) => {
-    console.log("Received: "+JSON.stringify(event,null,2));
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+  apiVersion: "2024-05-06"
+});
+const documentClient = DynamoDBDocumentClient.from(client);
+const tableName = "Cards";
+
+export const handler = async event => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  let statusCode = 200;
+  let body;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  
+  try {
+    const cards = await documentClient.send(
+      new ScanCommand({TableName: tableName})
+    );
+    body = cards.Items;
+  } catch (exception) {
+    console.error(exception);
+    statusCode = 500;
+    body = { "Message: ": exception };
+  } finally {
+    body = JSON.stringify(body);
+  }
+  return {
+    statusCode,
+    headers,
+    body,
+  };
 };
