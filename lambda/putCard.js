0e94f47ee6d342a2ab16163e8104847771677a2a
UHeeJoon <yhjyhj3855@gmail.com>
2024-05-10 13:45:52 +0900

    add: card update lambda

---
+++ lambda/putCard.js
@@ -0,0 +1,38 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({ region: "ap-northeast-2" });
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+const tableName = "Cards";
+
+export const handler = async (event) => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  try {
+    const requestJSON = JSON.parse(event.body);
+    const id = event.pathParameters.id;
+    const params = {
+      TableName: tableName,
+      Key: {
+        id,
+      },
+      UpdateExpression: "set #t = :title, #c = :category",
+      ExpressionAttributeNames: {
+        "#t": "title",
+        "#c": "category",
+      },
+      ExpressionAttributeValues: {
+        ":title": requestJSON.title,
+        ":category": requestJSON.category,
+      },
+      ReturnValues: "NONE",
+    };
+    await documentClient.send(new UpdateCommand(params));
+  } catch (err) {
+    console.log(err)
+  } 
+  return {
+    statusCode:200,
+  }
+};
